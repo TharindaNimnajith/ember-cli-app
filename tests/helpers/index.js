9f8593d2ec5ebd6b0b9ca594c94eebec97da35a2
Tharinda Nimnajith Rajapaksha <tharindarajapakshe@gmail.com>
2022-06-29 11:28:40 +0530

    reformat

--- tests/helpers/index.js
+++ tests/helpers/index.js
@@ -1,8 +1,8 @@
 import {
   setupApplicationTest as upstreamSetupApplicationTest,
   setupRenderingTest as upstreamSetupRenderingTest,
-  setupTest as upstreamSetupTest,
-} from 'ember-qunit';
+  setupTest as upstreamSetupTest
+} from "ember-qunit";
 
 // This file exists to provide wrappers around ember-qunit's / ember-mocha's
 // test setup functions. This way, you can easily extend the setup that is

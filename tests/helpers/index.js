c86a2331c2e50d0b79ae917b4cc4e2e651d51898
Tharinda Nimnajith Rajapaksha <tharindarajapakshe@gmail.com>
2022-06-29 12:23:05 +0530

    reformat

--- tests/helpers/index.js
+++ tests/helpers/index.js
@@ -1,8 +1,10 @@
+// noinspection ES6CheckImport,JSUnusedGlobalSymbols
+
 import {
   setupApplicationTest as upstreamSetupApplicationTest,
   setupRenderingTest as upstreamSetupRenderingTest,
-  setupTest as upstreamSetupTest
-} from "ember-qunit";
+  setupTest as upstreamSetupTest,
+} from 'ember-qunit';
 
 // This file exists to provide wrappers around ember-qunit's / ember-mocha's
 // test setup functions. This way, you can easily extend the setup that is
--- tests/test-helper.js
+++ tests/test-helper.js
@@ -1,9 +1,11 @@
-import Application from "ember-cli-app/app";
-import config from "ember-cli-app/config/environment";
-import * as QUnit from "qunit";
-import { setApplication } from "@ember/test-helpers";
-import { setup } from "qunit-dom";
-import { start } from "ember-qunit";
+// noinspection JSUnresolvedVariable,ES6CheckImport,NpmUsedModulesInstalled
+
+import Application from 'ember-cli-app/app';
+import config from 'ember-cli-app/config/environment';
+import * as QUnit from 'qunit';
+import { setApplication } from '@ember/test-helpers';
+import { setup } from 'qunit-dom';
+import { start } from 'ember-qunit';
 
 setApplication(Application.create(config.APP));
 
